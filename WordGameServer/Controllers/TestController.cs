ed15a5d48435eca9a81e0c9aedcd88036f4b9e26
amimelia <meliaamirani@gmail.com>
2017-07-31 22:12:48 +0400

    initial word game config.

---
+++ WordGameServer/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace WordGameServer.Controllers
+{
+    public class TestController : ApiController
+    {
+        string[] products = new string[]
+        {
+            "test", "test2", "test3"
+        };
+
+        public IEnumerable<string> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products[id];
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ WordGameServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WordGameServer.WebApiApplication" Language="C#" %>
